8c3c96f5b696f5bd026c9c2036a7da1cf0411066
faizinahsan <faizin110998@gmail.com>
2018-12-20 11:00:51 +0700

    Code For Arduino New

--- PWMForProjek/PWMForProjek.ino
+++ PWMForProjek/PWMForProjek.ino
@@ -3,117 +3,121 @@ const int E1 = 5;
 const int M1 = 4; 
 const int E2 = 6;                      
 const int M2 = 7;
-const int pingKanan = 9;
-const int pingKiri = 8;
-//const int pingDepan = 10;
-//const int buzzer = 7;
+const int pingPin = 9;
+const int pingPin2 = 8;
 char serialData;
-
-long pinPingFunc(int pinPing){
-  long duration, inches, cm;
-
-  pinMode(pinPing, OUTPUT);
-  digitalWrite(pinPing, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pinPing, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pinPing, LOW);
-
-  pinMode(pinPing, INPUT);
-  duration = pulseIn(pinPing, HIGH);
-
-  inches = microsecondsToInches(duration);
-  cm = microsecondsToCentimeters(duration);
-
-  delay(100);  
-  return cm;
-}
+//
+//long pinPingFunc(int pinPing){
+//  long duration, inches, cm;
+//
+//  pinMode(pinPing, OUTPUT);
+//  digitalWrite(pinPing, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(pinPing, HIGH);
+//  delayMicroseconds(5);
+//  digitalWrite(pinPing, LOW);
+//
+//  pinMode(pinPing, INPUT);
+//  duration = pulseIn(pinPing, HIGH);
+//
+//  inches = microsecondsToInches(duration);
+//  cm = microsecondsToCentimeters(duration);
+//
+//  delay(100);  
+//  return cm;
+//}
 
 void setup() 
 { 
     pinMode(M1, OUTPUT);   
     pinMode(M2, OUTPUT); 
-//    pinMode(buzzer, OUTPUT); 
     Serial.begin(9600);
 } 
 
 void kiri(){
     digitalWrite(M1,LOW);   
     digitalWrite(M2, LOW);       
-    analogWrite(E1, 175);   //PWM Speed Control
-    analogWrite(E2, 175);   //PWM Speed Control
-    delay(100);
+    analogWrite(E1, 255);   //PWM Speed Control
+    analogWrite(E2, 255);   //PWM Speed Control
+    delay(10);
 }
 
 void kanan(){
     digitalWrite(M1,HIGH);   
     digitalWrite(M2, HIGH);       
-    analogWrite(E1, 175);   //PWM Speed Control
-    analogWrite(E2, 175);   //PWM Speed Control
-    delay(100);
+    analogWrite(E1, 255);   //PWM Speed Control
+    analogWrite(E2, 255);   //PWM Speed Control
+    delay(10);
 }
 
 void diam(){
     analogWrite(E1, 0);   //PWM Speed Control
     analogWrite(E2, 0);   //PWM Speed Control
-    delay(100);
+    delay(10);
 }
 
 void loop() 
 { 
-  long cmKanan, cmKiri,cmDepan;
-  cmKanan = pinPingFunc(pingKanan);
-  cmKiri = pinPingFunc(pingKiri);
-//  cmDepan = pinPingFunc(pingDepan);
-  
-  Serial.print(cmKanan);
-  Serial.println("cm in ping Kanan");
-  Serial.print(cmKiri);
-  Serial.print("cm in ping Kiri");
-  Serial.println();
+//  long cm1,cm2;
+//  cm1 = pinPingFunc(pingPin);
+//  cm2 = pinPingFunc(pingPin2);
+//
+//  Serial.print(cm1);
+//  Serial.println("cm in ping 1");
+//  Serial.print(cm2);
+//  Serial.print("cm in ping 2");
+//  Serial.println();
 
+// Kodingan faizin
+//  if(cm1 <= 50 || cm2 <= 50){
+//    Serial.print("STOP");  
+//    diam();
+//  }else{
+//    kanan();
+//  }
+//  delay(10);
 
- // Kodingan berhasil
+//  if(cm1 <= 5 || cm2 <= 5){
+//    Serial.print("Berhenti");
+//  } else {
+//    if(Serial.available() > 0){
+//    serialData = Serial.read();
+//    Serial.print(serialData);
+//
+//    if(serialData == '1'){
+//      kanan();
+//    } else if(serialData == '0'){
+//      kiri();
+//    } else{
+//      diam();
+//    }
+//  }
+//  }
+
+// Kodingan berhasil
   if(Serial.available() > 0){
     serialData = Serial.read();
     Serial.print(serialData);
 
     if(serialData == '1'){
-      if(cmKiri <= 50){
-        diam();
-      } else{
-        kanan();
-        
-      }
+      kanan();
     } else if(serialData == '0'){
-      if(cmKanan <= 50){
-        diam();
-      } else{
-        kiri();
-      }
+      kiri();
     } else{
       diam();
-//      if(cmDepan <= 12){
-//        Tendang
-//          Serial.print("Tendang DIGG");
-//          Serial.println();
-//        digitalWrite(buzzer,HIGH);
-//        delay(200);
-//        digitalWrite(buzzer,LOW);
-//        delay(200);
-//      }
     }
+//   if(cm1 <= 25 || cm2 <= 25){
+//      Serial.print("STOP");  
+//      diam();
+//    }
+//    delay(10);
   }
-//  Serial.print(cmDepan);
-//  Serial.print("cm di depan");
-//  Serial.println();
-  delay(10);
 }
 
-long microsecondsToInches(long microseconds) {
-  return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29 / 2;
-}
+//long microsecondsToInches(long microseconds) {
+//  return microseconds / 74 / 2;
+//}
+//
+//long microsecondsToCentimeters(long microseconds) {
+//  return microseconds / 29 / 2;
+//}
